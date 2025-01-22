efc292844ffe8d224e4cbaa50615d7708a56002c
aken-you <kinsk2839@naver.com>
2025-01-22 16:12:22 +0900

    refactor: header 위치 이동

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
+import logoImg from "./icon.svg";
 import "./globals.css";
+import Image from "next/image";
 
 const notoSans = Noto_Sans_KR({
   variable: "--font-noto-sans",
@@ -22,7 +24,25 @@ export default function RootLayout({
       <head>
         <meta name="apple-mobile-web-app-title" content="MyWebSite" />
       </head>
-      <body className={`${notoSans.variable} antialiased`}>{children}</body>
+      <body className={`${notoSans.variable} min-h-screen antialiased`}>
+        <header className="bg-background/90 sticky top-0 z-50 flex w-full justify-center border-b px-4 backdrop-blur">
+          <div className="container flex h-14 items-center justify-between">
+            <div className="flex items-center gap-1">
+              <Image
+                src={logoImg}
+                alt="dev-stack-view-logo"
+                width="36"
+                height="36"
+              />
+              <span className="text-base font-extrabold text-gray-700">
+                dev-stack-view
+              </span>
+            </div>
+          </div>
+        </header>
+
+        <main className="flex-1">{children}</main>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,23 @@
-import Image from "next/image";
 import Link from "next/link";
-import logoImg from "./icon.svg";
 
 export default async function Home() {
   return (
-    <div className="flex min-h-screen flex-col">
-      <header className="flex w-full items-center justify-center px-4 py-4">
-        <div className="flex w-full max-w-screen-xl items-center justify-between">
-          <div className="flex items-center gap-1">
-            <Image
-              src={logoImg}
-              alt="dev-stack-view-logo"
-              width="36"
-              height="36"
-            />
-            <span className="text-base font-extrabold text-gray-700">
-              dev-stack-view
-            </span>
-          </div>
-        </div>
-      </header>
+    <div className="flex flex-col items-center px-8 pb-8">
+      <section className="flex w-full max-w-screen-lg flex-col items-center pb-36 pt-48">
+        <h1 className="text-5xl font-bold text-blue-800">
+          개발 스택을 한눈에 👀
+        </h1>
+        <p className="pt-3 text-2xl text-gray-600">
+          사용하는 기술 스택을 한눈에 볼 수 있는 이미지를 만들어보세요🔥
+        </p>
+      </section>
 
-      <main className="flex grow flex-col items-center">
-        <section className="flex w-full max-w-screen-lg flex-col items-center px-8 pb-36 pt-48">
-          <h1 className="text-5xl font-bold text-blue-800">
-            개발 스택을 한눈에 👀
-          </h1>
-          <p className="pt-3 text-2xl text-gray-600">
-            사용하는 기술 스택을 한눈에 볼 수 있는 이미지를 만들어보세요🔥
-          </p>
-        </section>
-
-        <Link
-          href="step1"
-          className="transform rounded-3xl bg-blue-500 px-10 py-4 text-xl font-semibold text-white shadow-xl transition-colors transition-transform hover:scale-110 hover:bg-blue-700 focus:scale-110"
-        >
-          지금 시작하기 🚀
-        </Link>
-      </main>
+      <Link
+        href="step1"
+        className="transform rounded-3xl bg-blue-500 px-10 py-4 text-xl font-semibold text-white shadow-xl transition-colors transition-transform hover:scale-110 hover:bg-blue-700 focus:scale-110"
+      >
+        지금 시작하기 🚀
+      </Link>
     </div>
   );
 }
