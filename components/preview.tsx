23ae887c81782262dbac70d89ee2705453319adc
aken-you <kinsk2839@naver.com>
2025-01-23 17:03:41 +0900

    refactor: Preview 스타일 변경

--- components/preview.tsx
+++ components/preview.tsx
@@ -19,21 +19,22 @@ export default function Preview({
   wrapperBoxStyle,
 }: PreviewProps) {
   return (
-    <Card className="bg-slate-100 p-6">
+    <Card className="overflow-x-auto bg-slate-100 p-6">
       <div className="flex min-h-[200px] items-center justify-center">
         <div
+          className="flex w-32 flex-col items-center gap-1 p-2"
           style={{
             ...wrapperBoxStyle,
-            display: "inline-block",
             border: wrapperBoxStyle?.borderColor
               ? `1px solid ${wrapperBoxStyle.borderColor}`
               : "none",
           }}
         >
           <div
-            className="mx-auto mb-3 h-24 w-24"
+            className="h-21 w-21 mx-auto mb-2"
             style={{
               ...iconBoxStyle,
+              margin: 0,
               display: "flex",
               alignItems: "center",
               justifyContent: "center",
@@ -42,7 +43,7 @@ export default function Preview({
                 : "none",
             }}
           >
-            <Image src={logoImg} width="40" height="40" alt="logo-image" />
+            <Image src={logoImg} width="80" height="80" alt="logo-image" />
           </div>
           <div className="text-center font-semibold">stack name</div>
         </div>
