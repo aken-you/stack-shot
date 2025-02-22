415b42526105c934fb759b2a0ebe2fd05fabbf9c
aken-you <kinsk2839@naver.com>
2025-02-22 23:27:28 +0900

    refactor: window가 undefined인지 확인하는 변수 생성

--- hooks/useSessionFormData.ts
+++ hooks/useSessionFormData.ts
@@ -1,27 +1,21 @@
 import type { IconBoxStyleType, Theme } from "@/types/style";
 import { INIT_ICON_BOX_STYLE } from "@/constants/step";
 
-interface SessionData {
+interface FormData {
   techStack: string[];
   theme: Theme;
   iconBoxStyle: IconBoxStyleType;
   title: string;
 }
 
-export default function useSessionFormData(): SessionData {
+export default function useSessionFormData(): FormData {
+  const isBrowser = typeof window !== "undefined";
+
   const session = {
-    techStack:
-      typeof window !== "undefined"
-        ? sessionStorage.getItem("techStack")
-        : null,
-    theme:
-      typeof window !== "undefined" ? sessionStorage.getItem("theme") : null,
-    iconBoxStyle:
-      typeof window !== "undefined"
-        ? sessionStorage.getItem("iconBoxStyle")
-        : null,
-    title:
-      typeof window !== "undefined" ? sessionStorage.getItem("title") : null,
+    techStack: isBrowser ? sessionStorage.getItem("techStack") : null,
+    theme: isBrowser ? sessionStorage.getItem("theme") : null,
+    iconBoxStyle: isBrowser ? sessionStorage.getItem("iconBoxStyle") : null,
+    title: isBrowser ? sessionStorage.getItem("title") : null,
   };
 
   const techStack = session.techStack
